74821bf7089834f72b028714929d5d5bf36c423a
Diogo Ferreira <diogoff94@gmail.com>
2017-06-02 19:11:23 +0100

    Now suggests the latest 5 commits on no hash

--- src/content-suggestors/git-commit-suggestor.js
+++ src/content-suggestors/git-commit-suggestor.js
@@ -1,14 +1,24 @@
 import { remote, ipcRenderer } from 'electron';
 
 const weaki = remote.getGlobal('instance');
-const regex = /#(\w+)\b/;
+const regex = /#(\w*)/;
+
+const LATEST_COMMITS_COUNT = 5;
 
 class GitCommitSuggestor {
+
+    static commitToSuggestion (commit) {
+        const smallHash = commit.hash.substring(commit.hash.length - 1 - 5);
+        return {
+            type: 0,
+            text: `${smallHash} ${commit.message}`
+        };
+    }
 
     constructor () {
         this.commits = [];
 
-        const updateCommits = () => weaki.git.getCommits().then(commits => this.commits = commits.all);
+        const updateCommits = () => weaki.git.getCommits().then(commits => this.commits = commits);
         ipcRenderer.on('application:workspace-changed', updateCommits);
         updateCommits();
     }
@@ -17,19 +27,22 @@ class GitCommitSuggestor {
         const suggestions = [];
         const match = regex.exec(textDescriptor.currentWord.text);
 
-        if (!match || !match[1])
+        if (!match)
             return suggestions;
 
         const inputHash = match[1];
-        for (let commit of this.commits) {
+        if (inputHash.length === 0) {
+            const latestCommits = this.commits.all.slice(0, LATEST_COMMITS_COUNT).map(GitCommitSuggestor.commitToSuggestion);
+            suggestions.push(...latestCommits);
+            return suggestions;
+        }
+
+        for (let commit of this.commits.all) {
             const smallHash = commit.hash.substring(commit.hash.length - 1 - 5);
             if (smallHash.indexOf(inputHash) !== 0)
                 continue;
 
-            suggestions.push({
-                type: 0,
-                text: `${smallHash} ${commit.message}`
-            });
+            suggestions.push(GitCommitSuggestor.commitToSuggestion(commit));
         }
 
         return suggestions;
